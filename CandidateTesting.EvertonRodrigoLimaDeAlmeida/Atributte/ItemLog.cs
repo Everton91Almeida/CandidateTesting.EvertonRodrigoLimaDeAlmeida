4b108fc18d48f792c0c9646dff9c302b59c92e48
Everton91Almeida <everton91.almeida@gmail.com>
2020-01-14 16:56:31 -0300

    Add project files.

---
+++ CandidateTesting.EvertonRodrigoLimaDeAlmeida/Atributte/ItemLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CandidateTesting.EvertonRodrigoLimaDeAlmeida.Atributte
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class ItemLog : Attribute
+    {
+        private string Name;
+
+        private int Position;
+
+        private Type Type;
+
+        private string RegexPattern;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Attribute name</param>
+        /// <param name="position">Position in layout, separated by |</param>
+        /// <param name="type">Data type</param>
+        /// <param name="pattern">Pattern of regular expression to read attribute</param>
+        public ItemLog(string name, int position, Type type, string pattern)
+        {
+            Name = name;
+            Position = position;
+            Type = type;
+            RegexPattern = pattern;
+        }
+    }
+}
---
+++ CandidateTesting.EvertonRodrigoLimaDeAlmeida/Program.cs
@@ -0,0 +1,30 @@
+﻿using CandidateTesting.EvertonRodrigoLimaDeAlmeida.BO;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CandidateTesting.EvertonRodrigoLimaDeAlmeida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = GetFileText().GetAwaiter().GetResult().Trim().Replace("\r", "").Split('\n');
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(new Log(item));
+            }
+
+            Console.Read();
+        }
+
+        private async static Task<string> GetFileText()
+        {
+            using (var http = new HttpClient())
+            {
+                return await http.GetStringAsync("https://s3.amazonaws.com/uux-itaas-static/minha-cdn-logs/input-01.txt");
+            }
+        }
+    }
+}
