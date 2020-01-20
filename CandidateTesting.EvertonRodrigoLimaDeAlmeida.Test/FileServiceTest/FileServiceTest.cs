4060317cd828abd78b1399de76d3b3324a5f7c1d
Everton91Almeida <everton91.almeida@gmail.com>
2020-01-20 15:17:26 -0300

    API test

---
+++ CandidateTesting.EvertonRodrigoLimaDeAlmeida.Test/FileServiceTest/FileServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using CandidateTesting.EvertonRodrigoLimaDeAlmeida.Domain.Interface.Service;
+using CandidateTesting.EvertonRodrigoLimaDeAlmeida.Domain.Service;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CandidateTesting.EvertonRodrigoLimaDeAlmeida.Test
+{
+    public class FileServiceTest
+    {
+        [Theory(DisplayName = "LogService GetString")]
+        [Trait("Service", "FileService")]
+        [InlineData("https://s3.amazonaws.com/uux-itaas-static/minha-cdn-logs/input-01.txt")]
+        public async Task FileService_GetString_ReturnLogStringFromService(string url)
+        {
+            IFileService fileService = new FileService();
+
+            var result = await fileService.GetString(url);
+
+            Assert.NotNull(result);
+        }
+    }
+}
